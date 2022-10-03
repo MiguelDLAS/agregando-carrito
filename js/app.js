58c5212cd6e51acc4d906a5cff47a8988dce1003
MIGUEL <mikiserrano2018@gmail.com>
2022-10-03 10:54:19 -0500

    ajustes btn

--- js/app.js
+++ js/app.js
@@ -4,14 +4,30 @@ const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
+let listadoCarrito = [];
+
 
 const agregarCurso =(e) => {
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')) {
-        console.log("Presionaste el boton");
+        const curso = e.target.parentElement.parentElement; 
+        const infocurso = {
+            imagen: curso.querySelector('img').src, 
+            nombre: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('p.precio').textContent,
+            cantidad: 1
 
+        }
+        agregarCarrito(infocurso);
     }
 }
+
+const agregarCarrito = curso => {
+    listadoCarrito = [...listadoCarrito, curso]
+    console.log(listadoCarrito);
+
+}
+
 
 const cargaEventListener = () => {
     //Agregar funcion de cursos al carrito
